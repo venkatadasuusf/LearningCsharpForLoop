981a42f64cf44a58f0477d51ca05c7dd2e46b21c
Venkata Dasu <vdasu@mail.usf.edu>
2019-09-08 13:10:09 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LearningCsharpForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine(j);
+                }
+                for (int k = 10; k >= 1; k--)
+                {
+                    Console.WriteLine(k);
+                }
+                               
+            }
+            
+        }
+    }
+}
